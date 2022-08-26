5800d90c86d3d217414495c05835ee2975c97984
AyalaStonic <Ayaladuvan876@gmail.com>
2022-08-25 23:07:15 -0400

    Public folder fixed

--- server.js
+++ server.js
@@ -1,68 +1,21 @@
-//declare variables and dependencies
-const express = require('express');
-const app = express();
-const { notes } = require('./db/notes');
-const fs = require('fs');
+// Require Dependencies
+const express = require("express");
+const fs = require("fs");
 const path = require('path');
 
-//port
-const PORT = process.env.PORT || 3001;
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-//middleware
-app.use(express.urlencoded({ extended: true}));
+// Setup data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
-//functions to be moved later
-function createNewNote(body, notesArr)
-{
-    const note = body;
-    notesArr.push(note);
-    fs.writeFileSync
-    (
-        path.join(__dirname, './db/notes.json'),
-        JSON.stringify({ notes: notesArr }, null, 2)
-    );
-    return note;
-};
+//Require routes file
+require('./routes/routes')(app);
 
-//api route(s)
-app.get('/api/notes', (req, res) =>
-{
-    res.json(notes);
-});
-
-//GET routes
-app.get('/', (req, res) =>
-{
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-app.get('/notes', (req, res) =>
-{
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-//send any weird request back to main notes page, NOT index.html
-app.get('*', (req, res) =>
-{
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-//POST route(s)
-app.post('/api/notes', (req, res) =>
-{
-    //use epoch ms to assign a unique id
-    const d = new Date();
-    let noteID = d.getTime();
-    req.body.id = noteID.toString();
-
-    //add to json file
-    const animal = createNewNote(req.body, notes);
-
-    res.json(req.body);
-});
-
-//confirmation of API
-app.listen(PORT, () =>
-{
-    console.log(`API server now on port ${PORT}!`)
-});
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
